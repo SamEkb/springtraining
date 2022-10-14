f14014fa0a2e5d846340657cfbbc9810fe361edd
Kilanov <sam.kilanoff@gmail.com>
2022-10-14 19:54:46 +0500

    spring-05-hw-jdbc add spring jdbc implementation of library and tests

--- spring-05-jdbc-hw/src/test/java/ru/skilanov/spring/config/TestContainersConfig.java
+++ spring-05-jdbc-hw/src/test/java/ru/skilanov/spring/config/TestContainersConfig.java
@@ -1,9 +1,10 @@
 package ru.skilanov.spring.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
         properties = {
                 "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///testdb"
         }
