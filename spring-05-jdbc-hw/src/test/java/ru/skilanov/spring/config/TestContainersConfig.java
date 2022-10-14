b45874bb254c8bacf9507c6db5fe39fe65572bba
Kilanov <sam.kilanoff@gmail.com>
2022-10-14 20:00:08 +0500

    spring-05-hw-jdbc simplify settings

--- spring-05-jdbc-hw/src/test/java/ru/skilanov/spring/config/TestContainersConfig.java
+++ spring-05-jdbc-hw/src/test/java/ru/skilanov/spring/config/TestContainersConfig.java
@@ -1,13 +1,9 @@
 package ru.skilanov.spring.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {
-                "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///testdb"
-        }
+        webEnvironment = SpringBootTest.WebEnvironment.NONE
 )
 public class TestContainersConfig {
 
